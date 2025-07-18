81024cfc391e266703c496fb4296823981db43ae
KHKAC <rudgnsrb888@naver.com>
2025-07-18 14:07:49 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,18 +2,8 @@
 {
     private static void Main(string[] args)
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        int i = 950;
-
-    
-=======
-        int i = 1812;
->>>>>>> 0ca4f3995d5443c79c880990c602744e3de1f9ea
-=======
-
         int i = 8475;
->>>>>>> eb20d000361439c9afaa3562d46fc01b1e2962af
+
 
         Console.WriteLine(i);
     }
